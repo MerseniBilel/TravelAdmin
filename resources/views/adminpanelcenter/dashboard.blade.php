187167d2608d96e9f0fb2a6aace3424e890e2339
MerseniBilel <bilelmerseni7016@gmail.com>
2020-09-06 13:39:08 +0200

    working on admin dashboard

--- resources/views/adminpanelcenter/dashboard.blade.php
+++ resources/views/adminpanelcenter/dashboard.blade.php
@@ -1,23 +1,9 @@
-@extends('layouts.app')
+@extends('adminpanelcenter.adminlayout')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+  <div class="content">
+      <div class="container-fluid">
+          <h4 class="page-title">Dashboard</h4>
+      </div>
+  </div>
 @endsection
